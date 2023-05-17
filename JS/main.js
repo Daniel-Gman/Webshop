6bdf238c0f49356ab8bf4719c8964fd42e3e97f8
Luc <34852@ma-web.nl>
2023-05-17 15:43:18 +0200

    finish animation

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,20 @@
-let handleClick = false;
+let handleClick = true;
 let myDiv = document.getElementById("switch");
 let animationDiv = document.getElementsByClassName("animation-saber")
 console.log(animationDiv)
 
 
-
 myDiv.addEventListener("click", function () {
-    if (handleClick == false) {
-        animationDiv.style.animationName = ""
-        handleClick = true;
-
-    } else if (handleClick == true) {
-        console.log("aan");
-        animationDiv.style.animationName = "saber-slider"
+    if (handleClick === true) {
+        for (let i = 0; i < animationDiv.length; i++) {
+            animationDiv[i].style.animationName = "saber-slider"
+        }
+        handleClick = false
+    } else if (handleClick === false) {
+        for (let i = 0; i < animationDiv.length; i++) {
+            animationDiv[i].style.animationName = "saber-slider-rev"
+        }
+        handleClick = true
     }
 });
 
