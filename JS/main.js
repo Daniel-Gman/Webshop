9504a3af5c9ee966fb01440c05934a27eef56a00
Daniel Vermeulen <34882@ma-web.nl>
2023-05-17 17:03:28 +0200

    sounds

--- JS/main.js
+++ JS/main.js
@@ -3,19 +3,28 @@ let myDiv = document.getElementById("switch");
 let animationDiv = document.getElementsByClassName("animation-saber")
 console.log(animationDiv)
 
+/*
+* Credits: Luc Zuidema, Oscar Slot, Daniel Vermeulen
+*/
 
 myDiv.addEventListener("click", function () {
     if (handleClick === true) {
         for (let i = 0; i < animationDiv.length; i++) {
             animationDiv[i].style.animationName = "saber-slider"
+
+            var audio = new Audio('JS/sounds/Star-wars-lightsaber-sound-effects.wav');
+            audio.loop = false;
+            audio.play();
         }
         handleClick = false
     } else if (handleClick === false) {
         for (let i = 0; i < animationDiv.length; i++) {
             animationDiv[i].style.animationName = "saber-slider-rev"
+
+            var audio = new Audio('JS/sounds/lightsaber2-105441.mp3');
+            audio.loop = false;
+            audio.play();
         }
         handleClick = true
     }
 });
-
-
