ddf2a5a91b4f569411094b77ddd8f8b5f8801dee
Daniel Vermeulen <34882@ma-web.nl>
2023-05-26 14:53:37 +0200

    winkelmand add noti

---
+++ JS/productPadawan.js
@@ -0,0 +1,91 @@
+const productTitle = document.getElementById('title');
+const circle1 = document.getElementById('circle1');
+const circle2 = document.getElementById('circle2');
+const circle3 = document.getElementById('circle3');
+const price = document.getElementById('price');
+const button = document.getElementById('button')
+const menu = document.getElementById('menu');
+const letter = document.getElementById('letter');
+const img = document.getElementById('img')
+let counter = 1;
+let buttoncount = true;
+
+circle2.style.background = 'white';
+circle3.style.background = 'white';
+
+circle1.addEventListener('click', function () {
+    circle1.style.background = '#ddc4f0b6';
+    productTitle.textContent = '(M)';
+    letter.textContent = 'M'
+    img.style.width = '36rem'
+    price.textContent = '€19,99';
+
+    circle2.style.background = 'white';
+    circle3.style.background = 'white';
+});
+
+circle2.addEventListener('click', function () {
+    circle2.style.background = '#ddc4f0b6';
+    productTitle.textContent = '(L)';
+    letter.textContent = 'L'
+    price.textContent = '€22,50';
+    img.style.width = '50rem'
+    circle1.style.background = 'white';
+    circle3.style.background = 'white';
+});
+
+circle3.addEventListener('click', function () {
+    circle3.style.background = '#ddc4f0b6';
+    productTitle.textContent = '(XL)';
+    letter.textContent = 'XL'
+    price.textContent = '€24,99';
+    img.style.width = '60rem'
+    circle2.style.background = 'white';
+    circle1.style.background = 'white';
+});
+
+button.addEventListener('click', function () {
+    alert("Item toegevoegd aan winkelwagen")
+    button.textContent = 'Item toegevoegd!'
+    buttoncount = false;
+    button.addEventListener('click', function () {
+        if (buttoncount === false) {
+            alert("Item is eerder toegevoegd, mocht dit een ongeluk zijn, check dan de winkelwagen!")
+        }
+    });
+})
+
+function changeProduct(counter) {
+    if (counter === 1) {
+        circle1.style.background = '#ddc4f0b6';
+        circle2.style.background = 'white';
+        circle3.style.background = 'white';
+        letter.textContent = 'M';
+        productTitle.textContent = '(M)';
+        price.textContent = '€19,99';
+        img.style.width = '36rem'
+    } else if (counter === 2) {
+        circle2.style.background = '#ddc4f0b6';
+        circle1.style.background = 'white';
+        circle3.style.background = 'white';
+        letter.textContent = 'L';
+        productTitle.textContent = '(L)';
+        price.textContent = '€22,50';
+        img.style.width = '50rem'
+    } else if (counter === 3) {
+        circle3.style.background = '#ddc4f0b6';
+        circle1.style.background = 'white';
+        circle2.style.background = 'white';
+        letter.textContent = 'XL';
+        productTitle.textContent = '(XL)';
+        price.textContent = '€24,99';
+        img.style.width = '60rem'
+    }
+
+    menu.addEventListener('click', function () {
+        counter = (counter % 3) + 1;
+        changeProduct(counter);
+    });
+}
+
+changeProduct(counter);
--- JS/productRed.js
+++ JS/productRed.js
@@ -6,17 +6,19 @@ const price = document.getElementById('price');
 const button = document.getElementById('button')
 const menu = document.getElementById('menu');
 const letter = document.getElementById('letter');
+const img = document.getElementById('img')
 let counter = 1;
+let buttoncount = true;
 
 circle2.style.background = 'white';
 circle3.style.background = 'white';
 
 circle1.addEventListener('click', function () {
     circle1.style.background = '#ddc4f0b6';
-    productTitle.textContent = 'Lightsaber Blauw (M)';
+    productTitle.textContent = '(M)';
     letter.textContent = 'M'
-
-    price.textContent = '€14,99';
+    img.style.width = '36rem'
+    price.textContent = '€24,99';
 
     circle2.style.background = 'white';
     circle3.style.background = 'white';
@@ -24,27 +26,34 @@ circle1.addEventListener('click', function () {
 
 circle2.addEventListener('click', function () {
     circle2.style.background = '#ddc4f0b6';
-    productTitle.textContent = 'Lightsaber Blauw (L)';
+    productTitle.textContent = '(L)';
     letter.textContent = 'L'
-    price.textContent = '€17,50';
-
+    price.textContent = '€27,50';
+    img.style.width = '50rem'
     circle1.style.background = 'white';
     circle3.style.background = 'white';
 });
 
 circle3.addEventListener('click', function () {
     circle3.style.background = '#ddc4f0b6';
-    productTitle.textContent = 'Lightsaber Blauw (XL)';
+    productTitle.textContent = '(XL)';
     letter.textContent = 'XL'
-    price.textContent = '€19,99';
-
+    price.textContent = '€29,99';
+    img.style.width = '60rem'
     circle2.style.background = 'white';
     circle1.style.background = 'white';
 });
 
 button.addEventListener('click', function () {
     alert("Item toegevoegd aan winkelwagen")
-});
+    button.textContent = 'Item toegevoegd!'
+    buttoncount = false;
+    button.addEventListener('click', function () {
+        if (buttoncount === false) {
+            alert("Item is eerder toegevoegd, mocht dit een ongeluk zijn, check dan de winkelwagen!")
+        }
+    });
+})
 
 function changeProduct(counter) {
     if (counter === 1) {
@@ -52,22 +61,25 @@ function changeProduct(counter) {
         circle2.style.background = 'white';
         circle3.style.background = 'white';
         letter.textContent = 'M';
-        productTitle.textContent = 'Lightsaber Blauw (M)';
-        price.textContent = '€14,99';
+        productTitle.textContent = '(M)';
+        price.textContent = '€24,99';
+        img.style.width = '36rem'
     } else if (counter === 2) {
         circle2.style.background = '#ddc4f0b6';
         circle1.style.background = 'white';
         circle3.style.background = 'white';
         letter.textContent = 'L';
-        productTitle.textContent = 'Lightsaber Blauw (L)';
-        price.textContent = '€17,50';
+        productTitle.textContent = '(L)';
+        price.textContent = '€27,50';
+        img.style.width = '50rem'
     } else if (counter === 3) {
         circle3.style.background = '#ddc4f0b6';
         circle1.style.background = 'white';
         circle2.style.background = 'white';
         letter.textContent = 'XL';
-        productTitle.textContent = 'Lightsaber Blauw (XL)';
-        price.textContent = '€19,99';
+        productTitle.textContent = '(XL)';
+        price.textContent = '€29,99';
+        img.style.width = '60rem'
     }
 
     menu.addEventListener('click', function () {
