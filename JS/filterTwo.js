7d92a62ab0a1b06635494cabeed21fa718e922f1
Daniel Vermeulen <34882@ma-web.nl>
2023-06-13 10:55:19 +0200

    filter2

---
+++ JS/filterTwo.js
@@ -0,0 +1,26 @@
+const price1 = document.getElementById("price1");
+const price2 = document.getElementById("price2");
+const price3 = document.getElementById("price3");
+const price4 = document.getElementById("price4");
+const price5 = document.getElementById("price5");
+const price6 = document.getElementById("price6");
+const price7 = document.getElementById("price7");
+const price8 = document.getElementById("price8");
+const price9 = document.getElementById("price9");
+const price10 = document.getElementById("price10");
+const price11 = document.getElementById("price11");
+const price12 = document.getElementById("price12");
+
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
+const img5 = document.getElementById("img5");
+const img6 = document.getElementById("img6");
+const img7 = document.getElementById("img7");
+const img8 = document.getElementById("img8");
+const img9 = document.getElementById("img9");
+const img10 = document.getElementById("img10");
+const img11 = document.getElementById("img11");
+const img12 = document.getElementById("img12");
+
